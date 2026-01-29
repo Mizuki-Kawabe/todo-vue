8b76b5907a2d57af07d28aa97f6373a5072b7053
Mizuki-Kawabe <111262336+Mizuki-Kawabe@users.noreply.github.com>
2026-01-29 18:12:43 +0900

    Creating our first Vue component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
-<script setup></script>
+<script setup>
+import ToDoItem from './components/ToDoItem.vue'
+</script>
 
 <template>
   <div id="app">
     <h1>To-Do List</h1>
+    <ul>
+      <li>
+        <to-do-item label="My ToDo Item" :done="true"></to-do-item>
+      </li>
+    </ul>
   </div>
 </template>
 
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="checkbox" id="todo-item" :checked="isDone" />
+    <label :for="id">{{ label }}</label>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    label: { required: true, type: String },
+    done: { default: false, type: Boolean },
+  },
+  data() {
+    return {
+      // use this to access a component's props and other properties from inside data
+      isDone: this.done,
+      id: `todo-${crypto.randomUUID()}`,
+    }
+  },
+}
+</script>
